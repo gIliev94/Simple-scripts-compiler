e6780bb4dea23772e39b6fe2c3ddbff8ae578161
Georgi Iliev <neokaozzz@gmail.com>
2016-01-12 22:58:51 +0200

    Append

---
+++ SimpleScript/src/simplescript/language/scripType/CommandFormats.java
@@ -0,0 +1,23 @@
+package simplescript.language.scripType;
+
+/**
+ * <h1>Resource class - lists all legal command formats.</h1>
+ * <p>
+ * </p>
+ * 
+ * @since 2016-01-12
+ * @author Georgi Iliev
+ * @version 1.0
+ */
+public class CommandFormats {
+    
+    public static final String CLICK_FORMAT = "^CLICK\\sLEFT$|RIGHT$|LEFTDOUBLE$";
+    public static final String MOVE_FORMAT = "^MOVE\\s[0-9]+\\s[0-9]+$";
+    public static final String PRESS_FORMAT = "^PRESS\\s\\w+$|\\w+\\+\\w+$";
+    public static final String DELAY_FORMAT = "^DELAY\\s[0-9]+$";
+    public static final String POINT_FORMAT = "^POINT\\s#[0123456789ABCDEF][0123456789ABCDEF][0123456789ABCDEF][0123456789ABCDEF][0123456789ABCDEF][0123456789ABCDEF]\\s[0-9][0-9][0-9]\\s[0-9][0-9][0-9]$";
+    public static final String LINE_FORMAT = "^LINE\\s#[0123456789ABCDEF][0123456789ABCDEF][0123456789ABCDEF][0123456789ABCDEF][0123456789ABCDEF][0123456789ABCDEF]\\s[0-9][0-9][0-9]\\s[0-9][0-9][0-9]\\s[0-9][0-9][0-9]\\s[0-9][0-9][0-9]\\s[0-1]$";
+    public static final String OPEN_FORMAT = "^OPEN\\s\\w+\\.\\w+$|\\w+\\.\\w+\\.\\w+$";
+    public static final String TEXT_FORMAT = "^TEXT\\s\\w+";
+
+}
